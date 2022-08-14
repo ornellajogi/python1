b770a6d028754ec35bb082f867a0caa4711086b3
ornellajogi <ornella.jogi@gmail.com>
2022-08-14 12:48:27 +0300

    SDA notes 2022.08.14 (2)

--- Joma_class/Programming_fundamentals_with_Python/variables.py
+++ Joma_class/Programming_fundamentals_with_Python/variables.py
@@ -6,10 +6,37 @@ if have f(x)=y
 Ex of variable = mouse setting scroll lines
 
 Whenever there is smth that can change, it is a variable
+
+print calls the function print, so print() is a function
+you can put input inside your function, that input can be a string or number
+another function we learned is type(), put in a number, string, etc, spits out output = class
 """
 
+print("Hello")
+print(5)
+print(type("I'm a string"))
+print(type(True))
+
+"""
+Anatomy of a function
+picture of it in notes
+ 
+def hello(name):
+    greet = "Hi! " + name
+    print(greet)
+    
+hello("Jonathan")
+
+you use def to define function, and the 3 lines are function definition
+"""
+
+def hello(name):
+    greet = "Hi! " + name
+    print(greet)
 
 
+hello("Jonathan")
+hello("rabbit")
 
 
 
